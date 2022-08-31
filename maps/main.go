1407b178f474e699d73e25c0a1953878bc15115f
Bagas Irwansyah <bagasirwansyah@yahoo.com>
2022-08-31 23:24:28 +0700

    [add]: map manipulation intro

---
+++ maps/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+	}
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
+
+	fmt.Println(colors)
+
+}
